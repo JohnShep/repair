9989ad735d12ae99ca62d3bd234575afacf1425e
Jason Downing <jason_downing@student.uml.edu>
2015-09-17 15:06:50 -0400

    quick fix to documentation

--- js/random_JT.js
+++ js/random_JT.js
@@ -1,3 +1,9 @@
+/*
+    Sources:
+    http://jsbin.com/ExeHoHu/1/edit?html,js,output
+    https://stackoverflow.com/questions/11722400/programmatically-change-the-src-of-an-img-tag
+    http://www.w3schools.com/jsref/event_onload.asp
+*/
 var imagesArray = ["img/JT_BRUINS.jpg", "img/JT_CAPECOD.jpg", "img/JT_CAPECOD2.jpg", "img/JT_Cropped.jpg",
 "img/JT_FLIPOFF.jpg", "img/JT_HAT.jpg", "img/JT_PRO.jpg", "img/JT_SHEPPLE.jpeg", "img/JT_SMILE.jpg", "img/JT_Yawn.jpg"];
 
